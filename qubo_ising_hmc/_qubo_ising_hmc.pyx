bb41a8960ae253e6d6aa147e1cc74607c42c66e5
Chris <chriskanii@gmail.com>
2020-03-02 11:00:21 -0800

    Started to wrap input

--- qubo_ising_hmc/_qubo_ising_hmc.pyx
+++ qubo_ising_hmc/_qubo_ising_hmc.pyx
@@ -26,16 +26,20 @@ cdef class Ising:
 
     def __cinit__(
         self,
-        const int Lambda_in,  # total number of sites (= L^dim)
         const vector[vector[double]] K_in,  # connectivity matrix (actually stores inverse of connectivity matrix)
         const vector[double] h_in, # external h-field
         const double mathcalE_in,  # this is an overall shift to the Hamiltonian
-        const double C_in, # mass term to regulate the connectivity matrix
         const double beta_in, # self explanatory (incorporates beta)
         const double mass_in,
         const int MDsteps_in,
         const int ergJumps_in
     ):
+        Lambda_in = len(h_in)
+
+        assert len(K_in) == Lambda_in
+
+        C_in = 6
+
         self._cobj = new ising(
             Lambda_in,
             K_in,
