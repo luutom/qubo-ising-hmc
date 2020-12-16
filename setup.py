322e5ec691e8a0b8e9bacc124c15ea934e18a1cd
cchang5 <chiachang@berkeley.edu>
2020-12-16 16:06:12 -0500

    try this

--- setup.py
+++ setup.py
@@ -4,7 +4,11 @@
 __author__ = "None"
 __version__ = "0.1.0"
 
-from os import path
+from os import path, environ
+import shutil
+import subprocess
+import pathlib
+import platform
 from distutils.extension import Extension
 
 from setuptools import setup, find_packages
@@ -20,12 +24,48 @@ SOURCES = [
     path.join(SRC, "_qubo_ising_hmc.pyx"),
 ]
 
+def findgccbin():
+    gcc = environ.get("CC")
+    if gcc is None:
+        gcc = shutil.which("gcc")
+        if gcc is None:
+            raise SystemError("Missing gcc, add to path or set env CC")
+    print("Found gcc at ", gcc)
+    # do test run of gcc
+    cmd = gcc + " -v"
+    ps = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()[0].decode("utf-8").split("\n")
+    if ps[0] != "Using built-in specs.":
+        raise SystemError("Requires gcc. Exiting install")
+    p = pathlib.Path(gcc)
+    # set some associated env vars for make
+    environ["CC"] = p.as_posix()
+    environ["CXX"] = p.parents[0].joinpath('g++').as_posix()
+    # Fix failure on unbuntu
+    # https://stackoverflow.com/questions/45308426/distutils-partially-ignoring-cc-environment-variable-when-building-python-extens
+    if platform.system() != 'Darwin':
+        environ["LDSHARED"] = environ["CXX"] + " -std=c++14 -pthread -shared"
+    return p.parents[0]
+
+# collect paths to things we need
+gccbin = findgccbin()
+gcclib64 = gccbin.parents[0].joinpath('lib64')
+if not gcclib64.is_dir():
+    gcclib64 = gccbin.parents[0].joinpath('lib')
+
+# define include dirs
+GCCLIB64 = gcclib64.as_posix()
+
+ldirs=['src/', '/usr/local/lib', GCCLIB64]
+link_list=['-fopenmp']
+
 EXTENSIONS = Extension(
     name="qihmc.qihmc_cc",
     sources=SOURCES,
+    library_dirs = ldirs,
     include_dirs=[SRC, get_include()],
     language="c++14",
-    extra_compile_args=["-O0", "-march=native", "-ffast-math", "-fopenmp"]
+    extra_compile_args=["-O0", "-march=native", "-ffast-math", "-fopenmp"],
+    extra_link_args = link_list,
 )
 
 with open(path.join(ROOT, "README.md"), encoding="utf-8") as inp:
